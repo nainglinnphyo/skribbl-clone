b6e09c6a772e1efbc084fb6055389cc02f1ea343
nainglinnphyo <nainglinnphyo24@gmail.com>
2024-03-27 17:20:59 +0630

    chore(app): add user bj

--- src/modules/socket/socket.gateway.ts
+++ src/modules/socket/socket.gateway.ts
@@ -80,12 +80,13 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
       case 'room-msg':
         guessed = payload.data.message === 'red';
         message = payload.data.message === 'red' ? 'You guessed word!' : payload.data.message;
-        this.server.to(payload.roomCode).emit(payload.event, { user:payload.data.user,msg: message, guessed });
+        this.server.to(payload.roomCode).emit(payload.event, { user: payload.data.user, msg: message, guessed });
         break;
       case 'drawing-data':
         drawingData = payload.data;
         this.server.to(payload.roomCode).emit(payload.event, { drawingData });
         break;
+
       default:
         this.server.to(payload.roomCode).emit(payload.event, { ...payload.data });
         break;
